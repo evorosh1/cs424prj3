dd4a90c9e5e5ca935bd831960afd8e917db0739e
liz <you@example.com>
2022-04-07 15:58:13 -0400

    prj3 start

---
+++ fruit_bot/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/liz/cs424prj3/fruit_bot/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/liz/fruitbot/devel;/opt/ros/melodic'.split(';') if '/home/liz/fruitbot/devel;/opt/ros/melodic' != '' else []
---
+++ fruit_bot/build/project3/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/liz/cs424prj3/fruit_bot/src/project3
+# Build directory: /home/liz/cs424prj3/fruit_bot/build/project3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
